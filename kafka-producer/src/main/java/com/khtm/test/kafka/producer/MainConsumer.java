2d6b259f683a1edbb3ee1c5f70478e3da19e857b
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-11-23 15:20:56 +0330

    Consumer class has been completed.

--- kafka-producer/src/main/java/com/khtm/test/kafka/producer/MainConsumer.java
+++ kafka-producer/src/main/java/com/khtm/test/kafka/producer/MainConsumer.java
@@ -5,8 +5,11 @@ public class MainConsumer {
 
     public static void main(String[] args) {
         String server = "10.12.47.125:9092";
-        String groupId = "some_application";
-        String topic = "user_registered";
+        //String groupId = "some_application";
+        //String topic = "user_registered";
+
+        String groupId = "some_tes";
+        String topic = "MCB.NAVACO.JPOS.M1100";
 
         new Consumer(server, groupId, topic).run();
     }
